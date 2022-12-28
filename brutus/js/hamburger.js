2702868ad8796b8a6d1149269bc095c385eb7724
Aris Panagiotides <arispan94@yahoo.gr>
2022-12-28 19:33:23 +0200

    Moved functionality to navigation.js

--- brutus/js/hamburger.js
+++
@@ -1,6 +0,0 @@
-var hamburgerContainer = document.getElementsByClassName("hamburger-container");
-console.log(hamburgerContainer);
-hamburgerContainer[0].addEventListener("click", function() {
-    this.classList.toggle("change-hamburger");
-    console.log("Changed");
-});
