7a9a39ce731fa7141ce5e18789726817e4682aad
Aris Panagiotides <arispan94@yahoo.gr>
2023-01-01 23:59:50 +0200

    Enqueued span-after-language.js script

--- brutus/functions.php
+++ brutus/functions.php
@@ -142,6 +142,7 @@ function brutus_scripts() {
 	wp_style_add_data( 'brutus-style', 'rtl', 'replace' );
 
 	wp_enqueue_script( 'brutus-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script('span-after-language-nav', get_template_directory_uri() . '/js/span-after-language.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
