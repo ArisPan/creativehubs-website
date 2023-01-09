26f3d933ba75d4cd2a3577883049ce6c8cee5d9c
Aris Panagiotides <arispan94@yahoo.gr>
2023-01-10 00:34:49 +0200

    Initial Commit

---
+++ brutus/page-catalog.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template file for Catalog page in english.
+ * 
+ * @package brutus
+ */
+
+get_header();
+?>
+
+<main id="primary" class="site-main">
+    <h1>Digital Catalog</h1>
+</main> <!-- .site-main -->
+
+<?php
+get_footer();
