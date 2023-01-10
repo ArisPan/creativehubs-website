92ef202ade5513cfda087018ae9b8647ff5daad5
Aris Panagiotides <arispan94@yahoo.gr>
2023-01-10 14:55:58 +0200

    Initial Commit

---
+++ brutus/page-katalogos.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Template file for Catalog page in greek.
+ *
+ * This template is also assigned to Catalog page in english via
+ * functions.php:set_custom_templates(). We will be providing a translation
+ * through brutus.pot
+ *
+ * @package brutus
+ */
+
+get_header();
+?>
+
+<main id="primary" class="site-main">
+    
+</main> <!-- .site-main -->
+
+<?php
+get_footer();
