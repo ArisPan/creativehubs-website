2c4b5b51624b8c294af95ddb3a5be5ef801af5ee
Aris Panagiotides <arispan94@yahoo.gr>
2023-01-10 17:12:19 +0200

    Added intro paragraphs and filter/search section outlines

--- brutus/page-katalogos.php
+++ brutus/page-katalogos.php
@@ -13,7 +13,29 @@ get_header();
 ?>
 
 <main id="primary" class="site-main">
-    
+    <div class="paragraph-container">
+        <h1 class="title"><?php esc_html_e( 'Τι;', 'brutus' ) ?></h1>
+        <p class="text"><?php esc_html_e( 'Ο ψηφιακός κατάλογος αποτελείται από ταξινομημένα και τεκμηριωμένα τεχνουργήματα της Περιφέρειας Ηπείρου.', 'brutus' ) ?></p>
+    </div>
+    <div class="paragraph-container">
+        <h1 class="title"><?php esc_html_e( 'Γιατί;', 'brutus' ) ?></h1>
+        <p class="text"><?php esc_html_e( 'Σκοπός του συλλεγόμενου υλικού είναι να βοηθήσει στον δημιουργικό σχεδιασμό. Τα μοτίβα που παρατηρούνται
+στην παραδοσιακή χειροτεχνία μπορούν να διευκολύνουν τη διαδικασία της μετα-παραγωγής με αποτέλεσμα την
+δημιουργία νέων αντικειμένων με ρίζες στους τρόπους μιας άλλης εποχής.', 'brutus' ) ?></p>
+    </div>
+    <div class="filters">
+        <div class="filters-responsive">
+            <!-- Placeholder -->
+        </div> <!-- .filters-responsive -->
+        <div class="form-group-flex filters-container">
+            <form class="form-group-flex form-group filters-group">
+
+            </form> <!-- .form-group.filters-group -->
+            <form class="form-group-flex form-group search-group">
+
+            </form>
+        </div> <!-- .form-group -->
+    </div> <!-- .filters -->
 </main> <!-- .site-main -->
 
 <?php
