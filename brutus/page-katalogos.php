c29fc6c1c7ce1966bfe98826314127c958370387
Aris Panagiotides <arispan94@yahoo.gr>
2023-01-13 00:16:26 +0200

    Added ul lists for dropdown filters

--- brutus/page-katalogos.php
+++ brutus/page-katalogos.php
@@ -34,26 +34,56 @@ get_header();
             <form class="form-group-flex form-group filters-group">
                 <div class="form-subgroup filters-subgroup">
                     <div name="origin", id="origin", class="selected-option-label"><?php esc_html_e( 'Προέλευση', 'brutus' ) ?></div>
-                </div> <!-- .form-subgroup subgroup -->
+                    <ul class="selected-dropdown">
+                        <li value="placeholder1">Placeholder 1</li>
+                        <li value="placeholder2">Placeholder 2</li>
+                        <li value="placeholder3">Placeholder 3</li>
+                        <li value="placeholder4">Placeholder 4</li>
+                        <li value="placeholder5">Placeholder 5</li>
+                        <li value="placeholder6">Placeholder 6</li>
+                        <li value="placeholder7">Placeholder 7</li>
+                        <li value="placeholder8">Placeholder 8</li>
+                        <li value="placeholder9">Placeholder 9</li>
+                        <li value="placeholder10">Placeholder 10</li>
+                    </ul> <!-- .selected-dropdown -->
+                </div> <!-- .form-subgroup.subgroup -->
                 <div class="form-subgroup filters-subgroup">
                     <div name="period", id="period", class="selected-option-label"><?php esc_html_e( 'Περίοδος', 'brutus' ) ?></div>
-                </div> <!-- .form-subgroup subgroup -->
+                    <ul class="selected-dropdown">
+                        <li value="placeholder1">Placeholder 1</li>
+                        <li value="placeholder2">Placeholder 2</li>
+                        <li value="placeholder3">Placeholder 3</li>
+                        <li value="placeholder4">Placeholder 4</li>
+                        <li value="placeholder5">Placeholder 5</li>
+                        <li value="placeholder6">Placeholder 6</li>
+                        <li value="placeholder7">Placeholder 7</li>
+                        <li value="placeholder8">Placeholder 8</li>
+                        <li value="placeholder9">Placeholder 9</li>
+                        <li value="placeholder10">Placeholder 10</li>
+                    </ul> <!-- .selected-dropdown -->
+                </div> <!-- .form-subgroup.subgroup -->
                 <div class="form-subgroup filters-subgroup">
                     <div name="material", id="material", class="selected-option-label"><?php esc_html_e( 'Υλικό', 'brutus' ) ?></div>
-                </div> <!-- .form-subgroup subgroup -->
+                    <ul class="selected-dropdown">
+                        <li value="placeholder1">Placeholder 1</li>
+                        <li value="placeholder2">Placeholder 2</li>
+                        <li value="placeholder3">Placeholder 3</li>
+                        <li value="placeholder4">Placeholder 4</li>
+                        <li value="placeholder5">Placeholder 5</li>
+                        <li value="placeholder6">Placeholder 6</li>
+                        <li value="placeholder7">Placeholder 7</li>
+                        <li value="placeholder8">Placeholder 8</li>
+                        <li value="placeholder9">Placeholder 9</li>
+                        <li value="placeholder10">Placeholder 10</li>
+                    </ul> <!-- .selected-dropdown -->
+                </div> <!-- .form-subgroup.subgroup -->
                 <div class="form-subgroup search-subgroup">
                     <input class="search-input" type="text" placeholder="<?php esc_html_e( 'Αναζήτηση', 'brutus' ) ?>">
                     <input class="hidden-input" type="submit", value="Search">
                 </div>
             </form> <!-- .form-group-flex.form-group.filters-group -->
 
-            <!-- <form class="form-group-flex form-group search-group">
-                <div class="form-subgroup search-subgroup">
-                    <input class="search-input" type="text" placeholder="<?php esc_html_e( 'Αναζήτηση', 'brutus' ) ?>">
-                    <input class="hidden-input" type="submit", value="Search">
-                </div>
-            </form> .form-group-flex.form-group.search-group -->
-        </div> <!-- .form-group -->
+        </div> <!-- .form-group-flex.filters-container -->
     </div> <!-- .filters -->
 </main> <!-- .site-main -->
 
