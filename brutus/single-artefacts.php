3895447e0e823459862908e85218c6932f85548c
Aris Panagiotides <arispan94@yahoo.gr>
2023-01-18 15:11:50 +0200

    Added markup for artefact content

--- brutus/single-artefacts.php
+++ brutus/single-artefacts.php
@@ -1,19 +1,52 @@
 <?php
 /**
- * Template file for Artefact posts in greek.
+ * Template file for single Artefact post in greek.
  *
  * @package brutus
  */
 
 get_header();
 ?>
-<main id="primary" class="site-main">
-    <?php
+
+<?php
+    $origin_custom_field_name = 'Προέλευση';
+    $period_custom_field_name = 'Περίοδος';
+    $description_custom_field_name = 'Περιγραφή';
+    $material_custom_field_name = 'Υλικό';
+
     while ( have_posts() ) :
         the_post();
-    endwhile;
-    ?>
-    <h1><?php the_title(); ?></h1>
+?>
+<main id="primary" class="site-main">
+
+    <div class="img-container">
+        <?php the_post_thumbnail( 'thumbnail' ); ?>
+    </div> <!-- .img-container -->
+
+    <div class="fields-container">
+        <h1 class="artefact title"><?php the_title(); ?></h1>
+
+        <div class="field-container origin">
+            <h4 class="label"><?php esc_html_e( $origin_custom_field_name, 'brutus' ) ?></h4>
+            <p class="content"><?php esc_html_e( get_post_meta( get_the_ID(), $origin_custom_field_name, true ) ) ?></p>
+        </div> <!-- .field-container origin -->
+
+        <div class="field-container period">
+            <h4 class="label"><?php esc_html_e( $period_custom_field_name, 'brutus' ) ?></h4>
+            <p class="content"><?php esc_html_e( get_post_meta( get_the_ID(), $period_custom_field_name, true ) ) ?></p>
+        </div> <!-- .field-container period -->
+
+        <div class="field-container description">
+            <h4 class="label"><?php esc_html_e( $description_custom_field_name, 'brutus' ) ?></h4>
+            <p class="content"><?php esc_html_e( get_post_meta( get_the_ID(), $description_custom_field_name, true ) ) ?></p>
+        </div> <!-- .field-container description -->
+
+        <div class="field-container material">
+            <h4 class="label"><?php esc_html_e( $material_custom_field_name, 'brutus' ) ?></h4>
+            <p class="content"><?php esc_html_e( get_post_meta( get_the_ID(), $material_custom_field_name, true ) ) ?></p>
+        </div> <!-- .field-container material -->
+    </div> <!-- .fields-container -->
+    <?php endwhile; ?>
 </main> <!-- .site-main -->
 <?php
 get_footer();
