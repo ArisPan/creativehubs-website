9ff0e1736a11bbe879a0c9f364da15da40058ab9
Aris Panagiotides <arispan94@yahoo.gr>
2023-01-19 01:01:49 +0200

    Enqueued hover-artefact js script

--- brutus/functions.php
+++ brutus/functions.php
@@ -144,6 +144,7 @@ function brutus_scripts() {
 	wp_enqueue_script( 'brutus-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
 	wp_enqueue_script( 'span-after-language-nav', get_template_directory_uri() . '/js/span-after-language.js', array(), _S_VERSION, true );
 	wp_enqueue_script( 'catalog-filters', get_template_directory_uri() . '/js/catalog-filters.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'hover-artefact', get_template_directory_uri() . '/js/hover-artefact.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
