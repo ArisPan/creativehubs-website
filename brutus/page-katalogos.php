1e9a5da21463da0d8f4e12f9a7d1603ee4688d6e
Aris Panagiotides <arispan94@yahoo.gr>
2023-01-26 00:57:59 +0200

    Enclosed intro paragraphs in div container

--- brutus/page-katalogos.php
+++ brutus/page-katalogos.php
@@ -14,17 +14,18 @@ get_header();
 
 <main id="primary" class="site-main">
 
-    <!-- Intro paragraphs -->
-    <div class="paragraph-container">
-        <h1 class="title"><?php esc_html_e( 'Τι;', 'brutus' ) ?></h1>
-        <p class="text"><?php esc_html_e( 'Ο ψηφιακός κατάλογος αποτελείται από ταξινομημένα και τεκμηριωμένα τεχνουργήματα της Περιφέρειας Ηπείρου.', 'brutus' ) ?></p>
-    </div> <!-- .paragraph-container -->
-    <div class="paragraph-container">
-        <h1 class="title"><?php esc_html_e( 'Γιατί;', 'brutus' ) ?></h1>
-        <p class="text"><?php esc_html_e( 'Σκοπός του συλλεγόμενου υλικού είναι να βοηθήσει στον δημιουργικό σχεδιασμό. Τα μοτίβα που παρατηρούνται
-στην παραδοσιακή χειροτεχνία μπορούν να διευκολύνουν τη διαδικασία της μετα-παραγωγής με αποτέλεσμα την
-δημιουργία νέων αντικειμένων με ρίζες στους τρόπους μιας άλλης εποχής.', 'brutus' ) ?></p>
-    </div> <!-- .paragraph-container -->
+    <div id="intro-container">
+        <div class="paragraph-container">
+            <h1 class="title"><?php esc_html_e( 'Τι;', 'brutus' ) ?></h1>
+            <p class="text"><?php esc_html_e( 'Ο ψηφιακός κατάλογος αποτελείται από ταξινομημένα και τεκμηριωμένα τεχνουργήματα της Περιφέρειας Ηπείρου.', 'brutus' ) ?></p>
+        </div> <!-- .paragraph-container -->
+        <div class="paragraph-container">
+            <h1 class="title"><?php esc_html_e( 'Γιατί;', 'brutus' ) ?></h1>
+            <p class="text"><?php esc_html_e( 'Σκοπός του συλλεγόμενου υλικού είναι να βοηθήσει στον δημιουργικό σχεδιασμό. Τα μοτίβα που παρατηρούνται
+    στην παραδοσιακή χειροτεχνία μπορούν να διευκολύνουν τη διαδικασία της μετα-παραγωγής με αποτέλεσμα την
+    δημιουργία νέων αντικειμένων με ρίζες στους τρόπους μιας άλλης εποχής.', 'brutus' ) ?></p>
+        </div> <!-- .paragraph-container -->
+    </div> <!-- .intro-container -->
 
     <!-- Filters/Search Bar -->
     <div class="filters">
@@ -57,11 +58,12 @@ get_header();
                         <li class="period" data-value="1900AD"><?php esc_html_e( '20ος Αιώνας μ.Χ.', 'brutus' ) ?></li>
                         <li class="period" data-value="1800AD"><?php esc_html_e( '19ος Αιώνας μ.Χ.', 'brutus' ) ?></li>
                         <li class="period" data-value="1700AD"><?php esc_html_e( '18ος Αιώνας μ.Χ.', 'brutus' ) ?></li>
-                        <li class="period" data-value="1300AD"><?php esc_html_e( '13ος Αιώνας μ.Χ.', 'brutus' ) ?></li>
+                        <li class="period" data-value="1200AD"><?php esc_html_e( '13ος Αιώνας μ.Χ.', 'brutus' ) ?></li>
                         <li class="period" data-value="500AD"><?php esc_html_e( '6ος Αιώνας μ.Χ.', 'brutus' ) ?></li>
                         <li class="period" data-value="400AD"><?php esc_html_e( '5ος Αιώνας μ.Χ.', 'brutus' ) ?></li>
                         <li class="period" data-value="100AD"><?php esc_html_e( '2ος Αιώνας μ.Χ.', 'brutus' ) ?></li>
                         <li class="period" data-value="1AD"><?php esc_html_e( '1ος Αιώνας μ.Χ.', 'brutus' ) ?></li>
+                        <li class="period" data-value="100BC"><?php esc_html_e( '2ος Αιώνας π.Χ.', 'brutus' ) ?></li>
                         <li class="period" data-value="200BC"><?php esc_html_e( '3ος αιώνας π.Χ.', 'brutus' ) ?></li>
                         <li class="period" data-value="300BC"><?php esc_html_e( '4ος αιώνας π.Χ.', 'brutus' ) ?></li>
                         <li class="period" data-value="400BC"><?php esc_html_e( '5ος Αιώνας π.Χ.', 'brutus' ) ?></li>
@@ -92,7 +94,7 @@ get_header();
                 </div> <!-- .form-subgroup.subgroup -->
                 <div class="form-subgroup search-subgroup">
                     <input class="search-input" type="text" placeholder="<?php esc_html_e( 'Αναζήτηση', 'brutus' ) ?>">
-                    <input class="hidden-input" type="submit", value="Search">
+                    <input class="hidden-input" type="submit" value="Search">
                 </div>
             </form> <!-- .form-group-flex.form-group.filters-group -->
         </div> <!-- .form-group-flex.filters-container -->
