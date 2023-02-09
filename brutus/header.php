0adbcffc85f6ceca9c57881f07901a969eb07a83
Aris Panagiotides <arispan94@yahoo.gr>
2023-02-09 14:33:47 +0200

    Added Google verification meta tag

--- brutus/header.php
+++ brutus/header.php
@@ -15,6 +15,7 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="google-site-verification" content="F1CTt6SAn84XnDIvr8NdJUirLVloIvwnIZfFznuE2UM" />
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
